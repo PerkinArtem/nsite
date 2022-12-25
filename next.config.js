7b52f160cb8bcb8bfe96ac03a5ab3914170a392d
PerkinArtem <artem.perkin1999@gmail.com>
2022-12-25 20:20:57 +0200

    project setting, inline svg import support

--- next.config.js
+++ next.config.js
@@ -7,6 +7,13 @@ const nextConfig = {
   experimental: {
     appDir: true,
   },
+  webpack: (config, options) => {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ['@svgr/webpack'],
+    });
+    return config;
+  },
 }
 
 module.exports = nextConfig
